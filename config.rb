e7c53a0dc4c2d702fa992a54da9385f77d93ad5e
Bruno Mordente <bruno.alves@bhtec.com.br>
2014-08-29 11:20:33 -0300

    Fixed Bugs

---
+++ config.rb
@@ -0,0 +1,30 @@
+#! Require any additional compass plugins here.
+
+#Configurando o root
+http_path = "/"
+
+# Diretório para o CSS
+css_dir = "dist/css"
+
+# Diretório para o SASS
+sass_dir = "assets/sass"
+
+# Diretório para as Imagens
+images_dir = "dist/img"
+
+# Diretório para os Js
+javascripts_dir = "assets/js"
+
+# Diretório para as Webfonts
+# fonts_dir = "assets/webfonts"
+
+# Diretório dos sprites
+sprite_load_path = "dist/img"
+
+output_style = :compressed
+environment = :production
+
+relative_assets = true
+
+line_comments = false
+color_output = false
